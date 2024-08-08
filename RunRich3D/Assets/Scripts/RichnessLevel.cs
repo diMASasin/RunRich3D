f1df6e14da3c61c80b299e800a9563fca5867426
diMASasin <35368058+diMASasin@users.noreply.github.com>
2024-08-08 10:50:56 +0500

    Done

--- RunRich3D/Assets/Scripts/RichnessLevel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class RichnessLevel
-{
-    [field: SerializeField] public int TargetValue { get; private set; }
-    [field: SerializeField] public GameObject Skin { get; private set; }
-}
